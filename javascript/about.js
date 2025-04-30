7701fc0e3f1acff15cf305755ebd13eae31772ec
brenda-leeCee <40734342@live.napier.ac.uk>
2025-04-30 15:49:58 -0600

    about us

---
+++ javascript/about.js
@@ -0,0 +1,11 @@
+// Animate group photo on scroll
+window.addEventListener('scroll', () => {
+    const group = document.getElementById('groupPhotoWrapper');
+    const rect = group.getBoundingClientRect();
+  
+    if (rect.top < window.innerHeight - 100) {
+      group.classList.add('fade-in-zoom');
+      group.classList.remove('opacity-0', 'scale-95');
+    }
+  });
+  
